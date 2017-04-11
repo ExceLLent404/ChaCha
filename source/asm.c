5c8ad4a34ce93b75fa30937ee4490a1a2f7cb5ee
ExceLLent <zerkc_97@mail.ru>
2017-04-11 18:50:07 +0300

    Lengths of "veor" commands and lengths of some (only for ChaCha) "vadd" commands were reduced.

--- source/asm.c
+++ source/asm.c
@@ -23,25 +23,25 @@ void salsa_rowround_asm(uint32_t y[16])
 		"vadd.i32 q5, q0, q3\n\t"            // a + d;
 		"vshl.i32 q5, q5, #25\n\t"           // (a + d) >> 25;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q1, q1, q4\n\t"                // b ^= ...;
+		"veor q1, q4\n\t"                    // b ^= ...;
 		"vadd.i32 q4, q1, q0\n\t"            // b + a;
 		"vshl.i32 q4, q4, #9\n\t"            // (b + a) << 9;
 		"vadd.i32 q5, q1, q0\n\t"            // b + a;
 		"vshl.i32 q5, q5, #23\n\t"           // (b + a) >> 23;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q2, q2, q4\n\t"                // c ^= ...;
+		"veor q2, q4\n\t"                    // c ^= ...;
 		"vadd.i32 q4, q2, q1\n\t"            // c + b;
 		"vshl.i32 q4, q4, #13\n\t"           // (c + b) << 13;
 		"vadd.i32 q5, q2, q1\n\t"            // c + b;
 		"vshl.i32 q5, q5, #19\n\t"           // (c + b) >> 19;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q3, q3, q4\n\t"                // d ^= ...;
+		"veor q3, q4\n\t"                    // d ^= ...;
 		"vadd.i32 q4, q3, q2\n\t"            // d + c;
 		"vshl.i32 q4, q4, #18\n\t"           // (d + c) << 18;
 		"vadd.i32 q5, q3, q2\n\t"            // d + c;
 		"vshl.i32 q5, q5, #14\n\t"           // (d + c) >> 14;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q0, q0, q4\n\t"                // a ^= ...;
+		"veor q0, q4\n\t"                    // a ^= ...;
 		"vstm.32 %[y], {q0, q1, q2, q3}\n\t" 
 		:
 		: [y] "r" (y)
@@ -72,25 +72,25 @@ void salsa_columnround_asm(uint32_t x[16])
 		"vadd.i32 q5, q0, q3\n\t"            // a + d;
 		"vshl.i32 q5, q5, #25\n\t"           // (a + d) >> 25;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q1, q1, q4\n\t"                // b ^= ...;
+		"veor q1, q4\n\t"                    // b ^= ...;
 		"vadd.i32 q4, q1, q0\n\t"            // b + a;
 		"vshl.i32 q4, q4, #9\n\t"            // (b + a) << 9;
 		"vadd.i32 q5, q1, q0\n\t"            // b + a;
 		"vshl.i32 q5, q5, #23\n\t"           // (b + a) >> 23;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q2, q2, q4\n\t"                // c ^= ...;
+		"veor q2, q4\n\t"                    // c ^= ...;
 		"vadd.i32 q4, q2, q1\n\t"            // c + b;
 		"vshl.i32 q4, q4, #13\n\t"           // (c + b) << 13;
 		"vadd.i32 q5, q2, q1\n\t"            // c + b;
 		"vshl.i32 q5, q5, #19\n\t"           // (c + b) >> 19;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q3, q3, q4\n\t"                // d ^= ...;
+		"veor q3, q4\n\t"                    // d ^= ...;
 		"vadd.i32 q4, q3, q2\n\t"            // d + c;
 		"vshl.i32 q4, q4, #18\n\t"           // (d + c) << 18;
 		"vadd.i32 q5, q3, q2\n\t"            // d + c;
 		"vshl.i32 q5, q5, #14\n\t"           // (d + c) >> 14;
 		"vorr q4, q4, q5\n\t"                // () | ()
-		"veor q0, q0, q4\n\t"                // a ^= ...;
+		"veor q0, q4\n\t"                    // a ^= ...;
 		"vstm.32 %[y], {q0, q1, q2, q3}\n\t" 
 		:
 		: [y] "r" (y)
@@ -118,23 +118,23 @@ void chacha_diagonalround_asm(uint32_t y[16])
 		"vext.32 q2, q2, q2, #2\n\t"
 		"vext.32 q3, q3, q3, #3\n\t"
 		/* now q0-q3 are as we need */
-		"vadd.i32 q0, q0, q1\n\t"             // a += b;
-		"veor q3, q0, q3\n\t"                 // d ^= a;
+		"vadd.i32 q0, q1\n\t"                 // a += b;
+		"veor q3, q0\n\t"                     // d ^= a;
 		"vshl.i32 q4, q3, #16\n\t"            // d = d <<< 16;
 		"vshr.u32 q5, q3, #16\n\t"
 		"vorr q3, q4, q5\n\t"
-		"vadd.i32 q2, q2, q3\n\t"             // c += d;
-		"veor q1, q1, q2\n\t"                 // b ^= c;
+		"vadd.i32 q2, q3\n\t"                 // c += d;
+		"veor q1, q2\n\t"                     // b ^= c;
 		"vshl.i32 q4, q1, #12\n\t"            // b = b <<< 12;
 		"vshr.u32 q5, q1, #20\n\t"
 		"vorr q1, q4, q5\n\t"
-		"vadd.i32 q0, q0, q1\n\t"             // a += b;
-		"veor q3, q0, q3\n\t"                 // d ^= a;
+		"vadd.i32 q0, q1\n\t"                 // a += b;
+		"veor q3, q0\n\t"                     // d ^= a;
 		"vshl.i32 q4, q3, #8\n\t"             // d = d <<< 8;
 		"vshr.u32 q5, q3, #24\n\t"
 		"vorr q3, q4, q5\n\t"
-		"vadd.i32 q3, q3, q3\n\t"             // c += d;
-		"veor q2, q2, q3\n\t"                 // b ^= c;
+		"vadd.i32 q2, q3\n\t"                 // c += d;
+		"veor q1, q2\n\t"                     // b ^= c;
 		"vshl.i32 q4, q1, #7\n\t"             // b = b <<< 7;
 		"vshr.u32 q5, q1, #25\n\t"
 		"vorr q1, q4, q5\n\t"
@@ -154,23 +154,23 @@ void chacha_columnround_asm(uint32_t y[16])
 	// q0 = a, q1 = b  q2 = c, q3 = d
 	asm(
 		"vldm.32 %[y], {q0, q1, q2, q3}\n\t"
-		"vadd.i32 q0, q0, q1\n\t"             // a += b;
-		"veor q3, q0, q3\n\t"                 // d ^= a;
+		"vadd.i32 q0, q1\n\t"                 // a += b;
+		"veor q3, q0\n\t"                     // d ^= a;
 		"vshl.i32 q4, q3, #16\n\t"            // d = d <<< 16;
 		"vshr.u32 q5, q3, #16\n\t"
 		"vorr q3, q4, q5\n\t"
-		"vadd.i32 q2, q2, q3\n\t"             // c += d;
-		"veor q1, q1, q2\n\t"                 // b ^= c;
+		"vadd.i32 q2, q3\n\t"                 // c += d;
+		"veor q1, q2\n\t"                     // b ^= c;
 		"vshl.i32 q4, q1, #12\n\t"            // b = b <<< 12;
 		"vshr.u32 q5, q1, #20\n\t"
 		"vorr q1, q4, q5\n\t"
-		"vadd.i32 q0, q0, q1\n\t"             // a += b;
-		"veor q3, q0, q3\n\t"                 // d ^= a;
+		"vadd.i32 q0, q1\n\t"                 // a += b;
+		"veor q3, q0\n\t"                     // d ^= a;
 		"vshl.i32 q4, q3, #8\n\t"             // d = d <<< 8;
 		"vshr.u32 q5, q3, #24\n\t"
 		"vorr q3, q4, q5\n\t"
-		"vadd.i32 q3, q3, q3\n\t"             // c += d;
-		"veor q2, q2, q3\n\t"                 // b ^= c;
+		"vadd.i32 q2, q3\n\t"                 // c += d;
+		"veor q1, q2\n\t"                     // b ^= c;
 		"vshl.i32 q4, q1, #7\n\t"             // b = b <<< 7;
 		"vshr.u32 q5, q1, #25\n\t"
 		"vorr q1, q4, q5\n\t"
